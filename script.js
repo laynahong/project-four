cd6ba4b8adf0be3a13be133e6a565bb69b59e153
Layna Hong <laynahong@Laynas-MacBook-Air.local>
2020-11-21 11:34:32 -0500

    card

--- script.js
+++ script.js
@@ -64,3 +64,17 @@ window.onclick = function(event) {
 }
 
   // *modal* //
+
+  //* card flipping *//
+  function flip(event){
+	var element = event.currentTarget;
+	if (element.className === "card") {
+    if(element.style.transform == "rotateY(180deg)") {
+      element.style.transform = "rotateY(0deg)";
+    }
+    else {
+      element.style.transform = "rotateY(180deg)";
+    }
+  }
+};
+  //* card flipping *//
